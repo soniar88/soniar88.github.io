42abaad776bff4585385ec7db2dbc6314ddd743d
soniar88 <sore6683@colorado.edu>
2022-10-17 15:31:56 -0600

    many changes

---
+++ assignments/ica8/ica8.js
@@ -0,0 +1,28 @@
+let body = document.querySelector('div');
+body.addEventListener('click',giveAlert);
+
+function giveAlert() {
+    alert('this is an alert');
+    }
+
+    document.getElementById("h2").addEventListener("click", myFunction);
+
+$("h3").on("mouseover"),function(){
+  console
+}
+
+const button = document.querySelector('button');
+
+function clickButton() {
+
+}
+
+button.addEventListener("click",clickButton)
+
+const h3 = document.querySelector('h3');
+
+function h3mouse() {
+
+}
+
+button.addEventListener("mouseover",h3mouse)
