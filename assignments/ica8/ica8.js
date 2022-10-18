0a270d93274467ea287403695602948e5e737027
soniar88 <sore6683@colorado.edu>
2022-10-18 16:45:28 -0600

    many changes

--- assignments/ica8/ica8.js
+++ assignments/ica8/ica8.js
@@ -5,11 +5,11 @@ function giveAlert() {
     alert('this is an alert');
     }
 
-    document.getElementById("h2").addEventListener("click", myFunction);
+document.getElementById("h2").addEventListener("click", myFunction);
 
-var button = document.querySelector('button');
-var box = document.getElementsByClassName('box6');
 
-button.onclick = function changeColor(){
-  box.style.background ="blue";
-}
+body.addEventListener("click",changeColor)
+function changeColor(){
+  docB.style.background = 'blue';
+  }
+ document.getElementById("colorbox").addEventListener("click",changeColor);
