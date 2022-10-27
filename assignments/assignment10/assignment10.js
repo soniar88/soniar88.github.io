9329aa1f02ba521f153ca2a359e4b422e09a7a9c
soniar88 <sore6683@colorado.edu>
2022-10-27 15:31:06 -0600

    Update assignment10.js

---
+++ assignments/assignment10/assignment10.js
@@ -0,0 +1,27 @@
+/*problem 2 */
+{   "companyName"; "Tech Stars",
+    "website";"www.techstars.site",
+    "employees";[ /*problem 2 */
+        {
+            "firsName":"Sam",
+            "deparment":"Tech",
+            "designation":"Manager",
+            "saraly": 4000,
+            "raiseEligible":true},
+            
+            {"firsName":"Mary",
+            "deparment":"Finace",
+            "designation":"Trainee",
+            "saraly":18500,
+            "raiseEligible":true },
+
+            
+            {"firsName":"Bill",
+            "deparment":"HR",
+            "designation":"Executive",
+            "saraly":21200,
+            "raiseEligible":false},
+
+    
+
+}
