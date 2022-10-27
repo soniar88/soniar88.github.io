6f05eb41be45f5099b124fbd6ca320fec2721214
soniar88 <sore6683@colorado.edu>
2022-10-26 19:35:07 -0600

    Update ica10.js

--- assignments/ica10/ica10.js
+++ assignments/ica10/ica10.js
@@ -56,6 +56,20 @@ class Ball {
         this.x += this.velX;
         this.y += this.velY;
       }
+      collisionDetect() {
+        for (const ball of balls) {
+          if (this !== ball) {
+            const dx = this.x - ball.x;
+            const dy = this.y - ball.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+      
+            if (distance < this.size + ball.size) {
+              ball.color = this.color = randomRGB();
+            }
+          }
+        }
+      }
+      
       
 }
 
@@ -84,6 +98,7 @@ function loop() {
     for (const ball of balls) {
       ball.draw();
       ball.update();
+      ball.collisionDetect();
     }
   
     requestAnimationFrame(loop);
