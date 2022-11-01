549001cbea5e3812bd56b6c723bdff5e0d2dcede
soniar88 <sore6683@colorado.edu>
2022-11-01 10:38:36 -0600

    added ICA11 docs

---
+++ assignments/ica11/ica11.js
@@ -0,0 +1,32 @@
+
+document.addEventListener("onload", getQuote);
+const quoteButton = document.querySelector('.new-quote'); 
+quoteButton.addEventListener('click', getQuote);
+
+const endpoint = "https://api.whatdoestrumpthink.com/api/v1/quotes/random";
+
+async function getQuote(){
+  //console.log("test click worked");
+  let text = await fetch(endpoint);
+  let response = await text.text();
+  // .then(text => text.text());
+  //let message = response['message'];
+
+  let json_response = JSON.parse(response);
+  // console.log(json_response);
+  console.log(json_response['message']);
+  displayQuote(json_response['message']);
+}
+
+function displayQuote(x){
+    //const quoteBox = document.querySelector("#js-quote-text");
+    //console.log('display q working');
+    //quoteBox.innerText(x);
+
+   // const textMessage = document.createTextNode(x);
+
+   // quoteBox.appendChild(textMessage);
+    document.getElementById('js-quote-text').textContent = x;
+
+}
+getQuote();
