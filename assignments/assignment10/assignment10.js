1f8fac70b707d4339e38cecb6bb4bc188ff89b3e
soniar88 <sore6683@colorado.edu>
2022-10-31 23:47:39 -0600

    Update assignment10.js

--- assignments/assignment10/assignment10.js
+++ assignments/assignment10/assignment10.js
@@ -2,7 +2,7 @@
 const Sam = { "firsName":"Sam",
             "deparment":"Tech",
             "designation":"Manager",
-            "saraly": 4000,
+            "saraly": 40000,
             "raiseEligible":true}
             
 const Mary = {"firsName":"Mary",
@@ -24,6 +24,7 @@ const Mary = {"firsName":"Mary",
             "saraly":256200,
             "raiseEligible":false}
 
+console.log("PROBLEM 1")
 console.log(Sam,Mary,Bill,Anna);
 
 // Problem 2 
@@ -33,25 +34,47 @@ const company = {
         "website":"www.techstars.site",
         "employees":[Sam,Mary,Bill,Anna]     
         }
+console.log("PROBLEM 2")
 console.log(company);
 
 // Problem 3 
-sor
+console.log("PROBLEM 3")
 console.log(Anna);
 
 //Problem 4
-
-let workers = company['employees'];
-for (let i=0; i > workers.length;i++){
-    console.log(workers[i])
+workersList = company.employees;
+let totalSalary =0; 
+for (const worker of workersList){
+    totalSalary += worker.saraly;
 }
 
-
-
+console.log("PROBLEM 4");
+console.log(`The total saraly is $ ${totalSalary}` );
 
 //Problem 5     
-
-
+function newSalary(company,workersList){
+    for (const worker of workersList){
+        if (worker.raiseEligible === "true"){
+            worker.saraly = worker.saraly *1.1;
+            worker.raiseEligible = "false";
+        }
+    }
+}
+console.log("PROBLEM 5");
+newSalary(company,workersList);
+console.log(company.employees);
 //Problem 6
 
+const WFH = ['Anna','Sam'];
+for (const worker of workersList){
+    if (WFH.includes(worker.firsName)){
+        worker.WFH = "true";
+    }
+    else { 
+        worker.WFH = "false"
+    }
 
+}
+
+console.log("PROBLEM 6")
+console.log(company.employees)
