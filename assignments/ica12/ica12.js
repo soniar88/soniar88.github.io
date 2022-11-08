da44b4e4d85b5ec33fc07c3bfd7fe0a364aece5a
soniar88 <sore6683@colorado.edu>
2022-11-08 14:47:39 -0700

    ica 12 updated

--- assignments/ica12/ica12.js
+++ assignments/ica12/ica12.js
@@ -1,9 +1,62 @@
-const button = document.querySelector('.button');
+/// const button = document.querySelector('.button');
+//const output = document.querySelector('.output');
+//let phone_content = document.querySelector('.phone');
+
+//utton.addEventListener('click', updateOutput);
+
+//function updateOutput() {
+  //  output.textContent = phone_content.value;
+    //start 
+
+const minusButton = document.querySelector("minus-button").addEventListener('click',check);
+const plussButton = document.querySelector("plus-button").addEventListener('click', plus );
+const resetButton = document.querySelector("minus-button").addEventListener('click',reset);
+const submitButton = document.querySelector("minus-button").addEventListener('click',submit);
+
 const output = document.querySelector('.output');
-let phone_content = document.querySelector('.phone');
+const outputInt = parseInt(output)
 
-button.addEventListener('click', updateOutput);
+console.log(outputInt)
 
-function updateOutput() {
-    output.textContent = phone_content.value;
+function check(){
+    console.log("test")
+
 }
+
+
+function submit(){
+    alert(output.textContent);
+
+}
+
+function reset(){
+    output.i
+   const reserValue = '000000000';
+    output.textContent = resetValue;
+
+}
+
+function plus(){
+    output.textContent = 
+
+    if (outputInt > 99999999){
+
+    }
+}
+
+function minus(){
+
+    if (outputInt > 0 ){
+
+    }
+
+    }
+}
+
+
+function randomNumber(){
+    outputInt 
+    output.textContent = outputInt
+    
+}
+const outputSlider
