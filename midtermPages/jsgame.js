77f449c9eec19cdb1646a2cd898a992f19a811e0
soniar88 <sore6683@colorado.edu>
2022-12-10 15:06:11 -0700

    game

---
+++ midtermPages/jsgame.js
@@ -0,0 +1,29 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+
+function jump(){
+    if (character.classList != "jump"){
+        character.classList.add("jump");
+        
+        setTimeout(function(){
+            character.classList.remove("jump");},300);
+    }
+}
+
+
+
+
+document.addEventListener("keydown",function(event){
+    jump();
+});
+
+var checkStatus = setInterval(function(){
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+
+    if (blockLeft<50 && blockLeft>0 && characterTop >=140){
+        alert("Game over :(");
+
+
+    }},10);
+
